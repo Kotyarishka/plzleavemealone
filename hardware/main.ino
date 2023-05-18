37dd185e6b48d78a763b6c8d1a19df6891f4b089
Kaligoz <114657098+Kaligoz@users.noreply.github.com>
2023-05-18 12:36:34 +0300

    Update main.ino
    ladno.

--- hardware/main.ino
+++ hardware/main.ino
@@ -5,95 +5,84 @@
 
 #define LED_INDICATOR 5
 #define SOLENOID 26
-#define SS_PIN  21  /*Slave Select Pin*/
-#define RST_PIN 22  /*Reset Pin for RC522*/
+#define SS_PIN 21  /*Slave Select Pin*/
+#define RST_PIN 22 /*Reset Pin for RC522*/
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 #define ssid "225"
 #define password "225225225"
-#define serverAddress "http://192.168.0.149:3000/api/v1"
+#define serverAddress "http://192.168.0.150:3000/api/v1"
 
 #define lockId "lock1"
 
 WiFiClient wifiClient;
 HTTPClient httpInit;
 
-enum State {
-  connecting,
-  connected,
-  error
-} ledState;
+bool cardOverride = false;
 
-bool ledStatus = false;
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-  SPI.begin();          /*SPI communication initialized*/
-  mfrc522.PCD_Init();   /*RFID sensor initialized*/ 
-  Serial.println("Put your card to the reader...");
-  Serial.println();
-
+  SPI.begin();        /*SPI communication initialized*/
+  mfrc522.PCD_Init(); /*RFID sensor initialized*/
 
   WiFi.begin(ssid, password, 6);
 
-  ledState = connecting;
-
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting");
   }
   Serial.println("Connected");
   httpInit.begin(wifiClient, serverAddress "/lock/create");
-  
+
   httpInit.addHeader("Content-Type", "application/json");
-  int httpResponseCode = httpInit.POST("{\"lockId\": \"" lockId "\"}");
+  httpInit.POST("{\"lockId\": \"" lockId "\"}");
 
-  if (httpResponseCode == 200) {
-    ledState = connected;
-  } else if (httpResponseCode == 302) {
-    ledState = connected;
-  } else {
-    String serverResponse = httpInit.getString();
-
-    ledState = error;
-  };
-
-  pinMode(LED_INDICATOR, OUTPUT);
+  // pinMode(LED_INDICATOR, OUTPUT);
   pinMode(SOLENOID, OUTPUT);
 
-  digitalWrite(LED_INDICATOR, LOW);
+  // digitalWrite(LED_INDICATOR, LOW);
   digitalWrite(4, HIGH);
 
   httpInit.end();
 }
 
 int requestTimer = millis();
-void makeRequest() {
-  if (requestTimer >= millis()) {
+void makeRequest()
+{ 
+  Serial.println("make request 1");
+  if (requestTimer >= millis())
+  {
     return;
   };
+
+  if (cardOverride) {
+    return;
+  };
+  
+  Serial.println("make request 2");
 
   HTTPClient http;
 
   http.begin(wifiClient, serverAddress "/lock/" lockId);
   http.addHeader("Content-Type", "text/plain");
-  
+
   int httpCode = http.GET();
-  String response = http.getString();  
-  Serial.println(httpCode);
-  Serial.println(response);
-  if (httpCode == HTTP_CODE_OK) {
-    if (response == "locked") {
-      digitalWrite(LED_INDICATOR, HIGH);
+  String response = http.getString();
+  // Serial.println(httpCode);
+  // Serial.println(response);
+  if (httpCode == HTTP_CODE_OK)
+  {
+    if (response == "locked")
+    {
       digitalWrite(SOLENOID, HIGH);
-    } else if (response == "unlocked") {
-      digitalWrite(LED_INDICATOR, LOW);
+    }
+    else if (response == "unlocked")
+    {
       digitalWrite(SOLENOID, LOW);
     }
-
-  } else {
-    ledState = error;
   }
 
   http.end();
@@ -101,69 +90,73 @@ void makeRequest() {
   requestTimer = millis() + 100;
 };
 
-int updateLedTimer = millis();
-void updateLedState() {
-  
-  if (updateLedTimer >= millis()) {
+int cardTimer = millis();
+String decoupleId = "";
+void checkRFID()
+{
+  if (cardTimer > millis())
+  {
     return;
   }
 
-  if (ledState == error) {
-    if (ledStatus) {
-      digitalWrite(LED_INDICATOR, LOW);
-      updateLedTimer = millis() + 200;
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    cardTimer = millis() + 100;
+    cardOverride = false;
 
-      ledStatus = false;
-    } else {
-      digitalWrite(LED_INDICATOR, HIGH);
-      updateLedTimer = millis() + 200;
+    Serial.println("no new card avaibla");
+    return;
+  }
 
-      ledStatus = true;
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    cardTimer = millis() + 100;
+    cardOverride = false;
+
+    decoupleId = "";
+    return;
+  }
+
+  if (decoupleId == "")
+  {
+    // generate decoupleId from random string and numbers
+
+    for (int i = 0; i < 10; i++)
+    {
+      decoupleId += String(random(0, 10));
     }
+  }
+
+  String content;
+
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  content.toUpperCase();
+  Serial.print(content);
+  Serial.println(" rfid");
+    if (content.substring(1) == "8B 07 CC 22") /*UID for the Card/Tag we want to give access Replace with your card UID*/
+  {
+    requestTimer = millis() + 5000;
+    cardOverride = true;
+    
+    digitalWrite(SOLENOID, LOW);
+
   }
 }
 
-void loop() {
+void loop()
+{
+
+
   // Make request to server
   makeRequest();
 
-  // update led state
-  updateLedState();
+  // Check RFID
+  checkRFID();
 
-  if ( ! mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
-  }
-  /*Select Card*/
-  if ( ! mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  /*Show UID for Card/Tag on serial monitor*/
-  Serial.print("UID tag :");
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  Serial.print(content);
-  content.toUpperCase();
-  if (content.substring(1) == "8B 07 CC 22") /*UID for the Card/Tag we want to give access Replace with your card UID*/
-  {
-    Serial.println("Authorized access");  /*Print message if UID match with the database*/
-    Serial.println();
-    delay(500);
-    digitalWrite(SOLENOID, HIGH);  /*Led Turn ON*/
-    delay(2500);
-    digitalWrite(SOLENOID, LOW);
-  }
- else   {
-    Serial.println(" Access denied"); /*If UID do not match print message*/
-  }
+  Serial.println(cardOverride);  
 }
